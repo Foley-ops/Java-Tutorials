f1134265e9c5b350df3b271131d1dbe7515e4460
Nick Foley <115027712+Foley-ops@users.noreply.github.com>
2023-01-13 21:59:15 -0600

    Add payment schedule and final refactors

--- Mortgage Calc/Java_intro/src/Main.java
+++ Mortgage Calc/Java_intro/src/Main.java
@@ -9,9 +9,28 @@ public class Main {
         float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 30);
         byte years = (byte) readNumber("Period (years): ", 1, 30);
 
+        printMortgage(principal, annualInterest, years);
+
+        printPaymentSchedule(principal, annualInterest, years);
+    }
+
+    private static void printMortgage(int principal, float annualInterest, byte years) {
         double mortgage = calculateMortgage(principal, annualInterest, years);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("========");
         System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
+
+    private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("========");
+        for (short month = 1; month <= years * 12; month++) {
+            double balance = calculateBalance(principal, annualInterest, years, month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
     }
 
     public static double readNumber(String prompt, double min, double max) {
@@ -35,10 +54,24 @@ public class Main {
 
         float monthlyRate = (annualInterest / 100) / 12;
         int period = years * 12;
-
         double mortgage = principal * ((monthlyRate * (Math.pow((monthlyRate + 1), period))) / ((Math.pow((monthlyRate + 1), period)) - 1));
-
         return mortgage;
     }
 
+    public static double calculateBalance(
+            int principal,
+            float annualInterest,
+            byte years,
+            float numberOfPaymentsMade) {
+        int numberOfPayments = years * 12;
+        float monthlyRate = (annualInterest / 100) / 12;
+        int period = years * 12;
+
+        double balance = principal *
+                (Math.pow(1 + monthlyRate, numberOfPayments) - Math.pow(1 + monthlyRate, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyRate, numberOfPayments) - 1);
+        return balance;
+
+
+    }
 }
